41b874595a4c8c749d64511cd94982edf76318b7
johnthar929 <87939894+johnthar929@users.noreply.github.com>
2025-07-10 00:45:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,33 @@
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
-const io = require('socket.io')(http);
 
-// Serve static files (if needed)
-app.use(express.static('public'));
+const io = require('socket.io')(http, {
+  cors: {
+    origin: '*',   // <-- This line is important!
+    methods: ['GET', 'POST']
+  }
+});
+
+app.get('/', (req, res) => {
+  res.send('Socket.IO Server running!');
+});
 
 io.on('connection', (socket) => {
   console.log('A user connected:', socket.id);
 
   socket.on('typing', (username) => {
+    console.log(username + ' is typing');
     socket.broadcast.emit('typing', username);
   });
 
   socket.on('stop typing', (username) => {
+    console.log(username + ' stopped typing');
     socket.broadcast.emit('stop typing', username);
   });
 
   socket.on('disconnect', () => {
-    console.log('User disconnected:', socket.id);
+    console.log('A user disconnected:', socket.id);
   });
 });
 
